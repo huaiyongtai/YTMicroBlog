2061fa81898e82d909074daf2c76c7ecc1db8af2
HuaiYongtai <memorywarning@gmail.com>
2015-09-29 20:04:00 +0800

    自定义导航按钮

---
+++ 无法修盖/Main/Views/TitleBtn.h
@@ -0,0 +1,18 @@
+//
+//  TitleBtn.h
+//  无法修盖
+//
+//  Created by HelloWorld on 15/9/29.
+//  Copyright (c) 2015年 SearchPrefect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TitleBtn : UIButton
+
+
+
+
+
+
+@end
