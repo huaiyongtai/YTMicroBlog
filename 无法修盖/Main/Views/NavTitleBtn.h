b12b19b3c2a71e83ed0075be790df28655da7a4d
HuaiYongtai <memorywarning@gmail.com>
2015-09-30 15:32:42 +0800

    自定义导航按钮

---
+++ 无法修盖/Main/Views/NavTitleBtn.h
@@ -0,0 +1,13 @@
+//
+//  NavTitleBtn.h
+//  无法修盖
+//
+//  Created by HelloWorld on 15/9/30.
+//  Copyright (c) 2015年 SearchPrefect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavTitleBtn : UIButton
+
+@end
---
+++ 无法修盖/Main/Views/NavTitleBtn.m
@@ -0,0 +1,42 @@
+//
+//  NavTitleBtn.m
+//  无法修盖
+//
+//  Created by HelloWorld on 15/9/30.
+//  Copyright (c) 2015年 SearchPrefect. All rights reserved.
+//
+
+#import "NavTitleBtn.h"
+
+@implementation NavTitleBtn
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//- (CGRect)imageRectForContentRect:(CGRect)contentRect {
+//    
+//}
+//
+//- (CGRect)titleRectForContentRect:(CGRect)contentRect {
+//    
+//}
+
+
+
+
+
+@end
