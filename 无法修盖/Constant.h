854dca6c183feb1b5905ab95330c4165276fe7b3
无法修改 <memorywarning@gmail.com>
2015-11-24 18:46:32 +0800

    添加常量类

---
+++ 无法修盖/Constant.h
@@ -0,0 +1,18 @@
+//
+//  Constant.h
+//  无法修盖
+//
+//  Created by HelloWorld on 15/11/2.
+//  Copyright (c) 2015年 SearchPrefect. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//extern NSString * const ConstantSina
+
+
+@interface Constant : NSObject
+
+
+
+@end
---
+++ 无法修盖/Constant.m
@@ -0,0 +1,13 @@
+//
+//  Constant.m
+//  无法修盖
+//
+//  Created by HelloWorld on 15/11/2.
+//  Copyright (c) 2015年 SearchPrefect. All rights reserved.
+//
+
+#import "Constant.h"
+
+@implementation Constant
+
+@end
