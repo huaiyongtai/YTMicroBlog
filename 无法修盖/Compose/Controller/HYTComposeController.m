52f866d5d49516fa78b4950dc9aee7e9db36fd57
HelloWorld <helloworld@HelloWorlds-MacBook-Air.local>
2016-01-05 01:23:14 +0800

    键盘上方的toolbar

--- 无法修盖/Compose/Controller/HYTComposeController.m
+++ 无法修盖/Compose/Controller/HYTComposeController.m
@@ -27,12 +27,11 @@
 
     [self setupNavInfo];
     
-    [self setupToolbar];
+    /**注意:子控件添加的顺序，必须先添加自定义textView 再添加toolbar，若textView后添加 self.automaticallyAdjustsScrollViewInsets=YES将失效，并且可能会遮挡toolbar显示*/
     
     [self setupTextView];
     
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidShow:) name:UIKeyboardDidShowNotification object:nil];
+    [self setupToolbar];
 }
 
 - (void)dealloc {
@@ -84,15 +83,17 @@
     [self.view addSubview:textView];
     self.textView = textView;
     
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillChangeFrame:) name:UIKeyboardWillChangeFrameNotification object:nil];
+    
 }
 
 - (void)setupToolbar {
     
     HYTComposeToolbar *toolbar = [HYTComposeToolbar toolbar];
     CGFloat toolbarHeight = 44;
-    [toolbar setFrame:CGRectMake(0, 0, self.view.width, toolbarHeight)];
+    [toolbar setFrame:CGRectMake(0, self.view.height-toolbarHeight, self.view.width, toolbarHeight)];
     [toolbar setDelegate:self];
-//    [self.view addSubview:toolbar];
+    [self.view addSubview:toolbar];
     self.toolbar = toolbar;
 }
 
@@ -144,18 +145,33 @@
     NSLog(@"-----发送-----");
 }
 
-- (void)keyboardWillShow:(NSNotification *)notification {
+- (void)keyboardWillChangeFrame:(NSNotification *)notification {
+    /*
+     *  UIKeyboardAnimationCurveUserInfoKey = 7;
+     *  UIKeyboardAnimationDurationUserInfoKey = "0.25";
+     *  UIKeyboardBoundsUserInfoKey = "NSRect: {{0, 0}, {320, 253}}";
+     *  UIKeyboardCenterBeginUserInfoKey = "NSPoint: {160, 372}";
+     *  UIKeyboardCenterEndUserInfoKey = "NSPoint: {160, 353.5}";
+     *  UIKeyboardFrameBeginUserInfoKey = "NSRect: {{0, 264}, {320, 216}}";
+     *  UIKeyboardFrameEndUserInfoKey = "NSRect: {{0, 227}, {320, 253}}";
+     */
     
-    self.textView.inputAccessoryView = self.toolbar;
-    NSLog(@"");
-}
-
-- (void)keyboardDidShow:(NSNotification *)notification {
+    NSTimeInterval duration = [notification.userInfo[UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    UIViewAnimationOptions animationOption = [notification.userInfo[UIKeyboardAnimationCurveUserInfoKey] integerValue];
+    CGFloat endKeyboardY = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue].origin.y;
     
-//    self.textView.inputAccessoryView = nil;
-    CGFloat toolbarHeight = 44;
-    [self.textView setFrame:CGRectMake(0, self.view.height = toolbarHeight, self.view.width, toolbarHeight)];
-    NSLog(@"");
+    if (endKeyboardY >= SCREEN_HEIGHT) {
+        endKeyboardY = SCREEN_HEIGHT;
+    }
+    CGFloat endToolbarY = endKeyboardY - self.toolbar.height;
+    CGRect endToolbarRect = CGRectMake(0, endToolbarY, self.toolbar.width, self.toolbar.height);
+    [UIView animateWithDuration:duration
+                          delay:0
+                        options:animationOption
+                     animations:^{
+                         [self.toolbar setFrame:endToolbarRect];
+                     }
+                     completion:nil];
 }
 
 @end
